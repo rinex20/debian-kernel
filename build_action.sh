fecc34728b78b1189b55404366123265097f6f84
rinex20 <rinex20@me.com>
2022-11-17 17:01:31 +0800

    Update build_action.sh

--- build_action.sh
+++ build_action.sh
@@ -1,30 +1,37 @@
 #!/usr/bin/env bash
 
 # add deb-src to sources.list Ubuntu系统只需要把系统 apt 配置中的源码仓库注释取消掉即可
-# sed -i "/deb-src/s/# //g" /etc/apt/sources.list
+sed -i "/deb-src/s/# //g" /etc/apt/sources.list
 
 # install dep
 sudo apt update
 sudo apt install -y wget
 sudo apt build-dep -y linux
 
-# change dir to workplace
-cd "${GITHUB_WORKSPACE}" || exit
+mkdir /usr/local/ARM-toolchain
 
 # download linaro gcc
+cd /usr/local/ARM-toolchain
 wget http://releases.linaro.org/components/toolchain/binaries/6.3-2017.05/aarch64-linux-gnu/gcc-linaro-6.3.1-2017.05-x86_64_aarch64-linux-gnu.tar.xz
+xz -d gcc-linaro-6.3.1-2017.05-x86_64_aarch64-linux-gnu.tar.xz
+tar xvf gcc-linaro-6.3.1-2017.05-x86_64_aarch64-linux-gnu.tar
+export PATH=/usr/local/ARM-toolchain:${PATH}
+echo $PATH
+
+# change dir to workplace
+cd "${GITHUB_WORKSPACE}" || exit
 
 make ARCH=arm64 tinker_edge_r_defconfig CROSS_COMPILE=/media/edger/gcc-linaro-6.3.1-2017.05-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu- -j32
 make ARCH=arm64 rk3399pro-tinker_edge_r.img CROSS_COMPILE=/media/edger/gcc- linaro-6.3.1-2017.05-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu- -j32
 
 # build deb packages
 # 获取系统的 CPU 核心数，将核心数X2设置为编译时开启的进程数，以加快编译速度
-# CPU_CORES=$(($(grep -c processor < /proc/cpuinfo)*2))
-# make deb-pkg -j"$CPU_CORES"
+CPU_CORES=$(($(grep -c processor < /proc/cpuinfo)*2))
+make deb-pkg -j"$CPU_CORES"
 
 # move deb packages to artifact dir
-# cd ..
-# mkdir "artifact"
+cd ..
+mkdir "artifact"
 # 删除无用且巨大的调试包
 rm ./*dbg*.deb
 mv ./*.deb artifact/
